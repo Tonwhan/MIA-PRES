f4e8dec1de00e97ab21e64a25d934396b6d8ebec
tonwhan <157872916+Tonwhan@users.noreply.github.com>
2026-03-09 12:32:32 +0700

    feature: Initialize Supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:mia_pres/features/onboarding/presentation/welcome_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+// Supabase Initialize
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
+  final client = Supabase.instance.client;
+  debugPrint("Supabase Connected");
+  (client.auth.currentSession);
+
   runApp(const MainApp());
 }
 
@@ -17,7 +33,7 @@ class MainApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.white,
       ),
       debugShowCheckedModeBanner: false,
-      home: OnboardingPage(),
+      home: const OnboardingPage(),
     );
   }
 }

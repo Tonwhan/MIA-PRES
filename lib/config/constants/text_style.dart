2ad477460454a1fb8b03ce33c14d05774ea29618
tonwhan <157872916+Tonwhan@users.noreply.github.com>
2026-03-17 14:47:09 +0700

    feat(ui): implement global app theme and typography

--- lib/config/constants/text_style.dart
+++ lib/config/constants/text_style.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 
-class AppTextDark{
+class AppTextDark {
   static const TextStyle title1 = TextStyle(
-    fontSize: 38,
+    fontSize: 32,
     fontWeight: FontWeight.w800,
-    color: Colors.black,
   );
-}
-
-class AppTextLight{
-  static const TextStyle title1 = TextStyle(
-    fontSize: 38,
+  static const TextStyle title2 = TextStyle(
+    fontSize: 28,
     fontWeight: FontWeight.w800,
-    color: Color(0XFF666666),
+  );
+  static const TextStyle title3 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w800,
+  );
+  static const TextStyle subtitle0 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: Colors.white
+  );
+  static const TextStyle subtitle1 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: Color(0XFFCFCFCF),
+  );
+  static const TextStyle body = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
   );
 }
--- lib/config/theme/status_colors.dart
+++ lib/config/theme/status_colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const taken = Color(0xFF4CAF50); // Use for complete
-const dueNow = Color(0xFFFFC107); // Use for Take now
-const missed = Color(0xFFF44336); // Use for Missed
-const upcoming = Color(0xFF9E9E9E); // Use for Upcoming
+const taken = Color(0xFF85CC17);
+const now = Color(0xFFFFA407);
+const missed = Color(0xFFF4405F);
+const upcoming = Color(0xFF838383);

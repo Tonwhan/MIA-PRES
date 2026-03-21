467931d615dfd7206927f1e0bfa0747305f79faf
tonwhan <157872916+Tonwhan@users.noreply.github.com>
2026-03-21 17:49:56 +0700

    feat(ui): configure system status bar brightness

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pillpal/config/theme/colors_theme.dart';
+import 'package:pillpal/widgets/header_dashboard.dart';
+import 'package:pillpal/widgets/medication_tracker/midication_tracker.dart';
+import 'package:pillpal/widgets/scan_prescription_bar.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,13 +15,29 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [],
+    final bool isDarkBackground =
+        ThemeData.estimateBrightnessForColor(AppColors.backgroundColor) ==
+        Brightness.dark;
+
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarBrightness: isDarkBackground
+            ? Brightness.dark
+            : Brightness.light,
+        statusBarIconBrightness: isDarkBackground
+            ? Brightness.dark
+            : Brightness.light,
+        statusBarColor: Colors.transparent,
+      ),
+      child: Scaffold(
+        backgroundColor: AppColors.backgorundColor1,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: SafeArea(
+            child: Column(
+              children: [
+              ],
+            ),
           ),
         ),
       ),

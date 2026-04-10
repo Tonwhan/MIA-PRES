da50ff19f94cab73058d46f1fa41024024bfd23e
tonwhan <157872916+Tonwhan@users.noreply.github.com>
2026-04-10 14:44:55 +0700

    feat(ui): add more page, privacy page and terms page

--- lib/features/settings/presentation/pages/terms_page.dart
+++ lib/features/settings/presentation/pages/terms_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:pillpal/config/theme/colors_theme.dart';
 
 class Termspage extends StatelessWidget {
   const Termspage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.primary,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios_new_rounded, color: Colors.white),
+        ),
+      ),
+    );
   }
 }

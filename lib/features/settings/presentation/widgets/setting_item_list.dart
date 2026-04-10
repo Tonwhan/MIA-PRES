d7a1bba1ba4714b499b529249764490b825c4b0d
tonwhan <157872916+Tonwhan@users.noreply.github.com>
2026-04-10 14:33:17 +0700

    feature(ui): add setting item widget

--- lib/features/settings/presentation/widgets/setting_item_list.dart
+++ lib/features/settings/presentation/widgets/setting_item_list.dart
@@ -4,56 +4,75 @@ import 'package:pillpal/config/theme/colors_theme.dart';
 
 class SettingItemList extends StatelessWidget {
   final String title;
-  final String svgIcon;
+  final String? svgIcon;
   final Widget route;
+
   const SettingItemList({
     super.key,
     required this.title,
-    required this.svgIcon,
+    this.svgIcon,
     required this.route,
   });
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => route));
-      },
-      child: Row(
-        children: [
-          Container(
-            width: 32,
-            height: 32,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.white,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: AppColors.backgroundColor,
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => route),
+          );
+        },
+        splashFactory: NoSplash.splashFactory,
+        highlightColor: Colors.white.withValues(alpha: 0.1),
+        splashColor: Colors.transparent,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12.0,horizontal: 16),
+          child: Row(
+            children: [
+              if (svgIcon != null) ...[
+                Container(
+                  width: 32,
+                  height: 32,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(1.5),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: AppColors.backgroundColor,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Center(child: SvgPicture.asset(svgIcon!)),
+                      ),
+                    ),
+                  ),
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Center(child: SvgPicture.asset(svgIcon)),
+                const SizedBox(width: 12),
+              ],
+              Text(
+                title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
-            ),
+              const Spacer(),
+              const Icon(
+                Icons.arrow_forward_ios_rounded,
+                color: Colors.white54,
+                size: 16,
+              ),
+            ],
           ),
-          SizedBox(width: 8),
-          Text(
-            title,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          Spacer(),
-          Icon(Icons.arrow_forward_ios_rounded, color: Colors.white),
-        ],
+        ),
       ),
     );
   }
